4cccdca09b5a237c16e611d4895e0206fe34734c
Josue Rivera <prbassplayer@gmail.com>
2017-02-02 04:49:33 +0100

    mako: Disable Snap for now
      * Snap cannot initialize video recording right now
    
    Change-Id: Iaab0b5c63c6c4b8ad2b934d764e82d6ed80917be
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- device.mk
+++ device.mk
@@ -264,8 +264,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
 
 # Camera
-PRODUCT_PACKAGES += \
-    Snap
+#PRODUCT_PACKAGES += \
+#    Snap
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.legacyencoder=true \
