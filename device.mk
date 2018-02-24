dafeae5cbaf240c3f2ee5cebe36997d98f763a50
voron00 <alexey12424323@gmail.com>
2018-02-24 13:09:12 +0300

    mako: add DxHDCP conf from stock blobs
    Change-Id: I94788f9a79ae7839f5bbbe465a6377386fb7b478

--- device.mk
+++ device.mk
@@ -59,6 +59,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# DxHDCP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/DxHDCP.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/DxHDCP.cfg
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     bdAddrLoader \
