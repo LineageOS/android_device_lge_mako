6d2545c0065e92c4fb35c1e4343815ff8ab3f2c8
voron00 <alexey12424323@gmail.com>
2018-03-01 18:24:52 +0300

    mako: bind camera and omx hals
    Change-Id: I7d749288cadc50a4d1c75c0d17f338425315b6eb

--- device.mk
+++ device.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.mako \
     libmmcamera_interface2 \
