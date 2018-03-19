44cb9027eb8463729f263b6a259436082f6d595b
voron00 <alexey12424323@gmail.com>
2018-03-19 21:29:31 +0300

    mako: rewrite powerhal to use interactive touch, interaction boosts and freq limits
    Since we removed mpdecision, we have to rewrite our powerhal to handle boosts and limits without it. This initial implentation does the trick, but some more work is still needed.
    
    Change-Id: Ia8aa6918565415b9a71e242c17b656d07c42a155

--- power/power_mako.c
+++ power/power_mako.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,57 +36,13 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define STATE_ON "state=1"
-#define STATE_OFF "state=0"
-#define STATE_HDR_ON "state=2"
-#define STATE_HDR_OFF "state=3"
+#define BOOST_PATH "/sys/devices/system/cpu/cpufreq/interactive/boost"
+#define LIMIT_PATH "/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq"
 
-#define MAX_LENGTH         50
-#define BOOST_SOCKET       "/dev/socket/pb"
+#define NORMAL_FREQ "1512000"
+#define LIMIT_FREQ  "1026000"
 
-#define UEVENT_MSG_LEN 2048
-#define TOTAL_CPUS 4
-#define RETRY_TIME_CHANGING_FREQ 20
-#define SLEEP_USEC_BETWN_RETRY 200
-#define LOW_POWER_MAX_FREQ "1026000"
-#define LOW_POWER_MIN_FREQ "384000"
-#define NORMAL_MAX_FREQ "1512000"
-#define UEVENT_STRING "online@/devices/system/cpu/"
-
-static int client_sockfd;
-static struct sockaddr_un client_addr;
-static int last_state = -1;
-
-static struct pollfd pfd;
-static char *cpu_path_min[] = {
-    "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq",
-    "/sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq",
-    "/sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq",
-    "/sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq",
-};
-static char *cpu_path_max[] = {
-    "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-    "/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq",
-    "/sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq",
-    "/sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq",
-};
-static bool freq_set[TOTAL_CPUS];
 static bool low_power_mode = false;
-static pthread_mutex_t low_power_mode_lock = PTHREAD_MUTEX_INITIALIZER;
-
-static void socket_init()
-{
-    if (!client_sockfd) {
-        client_sockfd = socket(PF_UNIX, SOCK_DGRAM, 0);
-        if (client_sockfd < 0) {
-            ALOGE("%s: failed to open: %s", __func__, strerror(errno));
-            return;
-        }
-        memset(&client_addr, 0, sizeof(struct sockaddr_un));
-        client_addr.sun_family = AF_UNIX;
-        snprintf(client_addr.sun_path, UNIX_PATH_MAX, BOOST_SOCKET);
-    }
-}
 
 static int sysfs_write(const char *path, char *s)
 {
@@ -100,6 +57,7 @@ static int sysfs_write(const char *path, char *s)
     }
 
     len = write(fd, s, strlen(s));
+
     if (len < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error writing to %s: %s\n", path, buf);
@@ -110,236 +68,16 @@ static int sysfs_write(const char *path, char *s)
     return 0;
 }
 
-static int uevent_event()
-{
-    char msg[UEVENT_MSG_LEN];
-    char *cp;
-    int n, cpu, ret, retry = RETRY_TIME_CHANGING_FREQ;
-
-    n = recv(pfd.fd, msg, UEVENT_MSG_LEN, MSG_DONTWAIT);
-    if (n <= 0) {
-        return -1;
-    }
-    if (n >= UEVENT_MSG_LEN) {   /* overflow -- discard */
-        return -1;
-    }
-
-    cp = msg;
-
-    if (strstr(cp, UEVENT_STRING)) {
-        n = strlen(cp);
-        errno = 0;
-        cpu = strtol(cp + n - 1, NULL, 10);
-
-        if (errno == EINVAL || errno == ERANGE || cpu < 0 || cpu >= TOTAL_CPUS) {
-            return -1;
-        }
-
-        pthread_mutex_lock(&low_power_mode_lock);
-        if (low_power_mode && !freq_set[cpu]) {
-            while (retry) {
-                sysfs_write(cpu_path_min[cpu], LOW_POWER_MIN_FREQ);
-                ret = sysfs_write(cpu_path_max[cpu], LOW_POWER_MAX_FREQ);
-                if (!ret) {
-                    freq_set[cpu] = true;
-                    break;
-                }
-                usleep(SLEEP_USEC_BETWN_RETRY);
-                retry--;
-           }
-        } else if (!low_power_mode && freq_set[cpu]) {
-             while (retry) {
-                  ret = sysfs_write(cpu_path_max[cpu], NORMAL_MAX_FREQ);
-                  if (!ret) {
-                      freq_set[cpu] = false;
-                      break;
-                  }
-                  usleep(SLEEP_USEC_BETWN_RETRY);
-                  retry--;
-             }
-        }
-        pthread_mutex_unlock(&low_power_mode_lock);
-    }
-    return 0;
-}
-
-void *thread_uevent(__attribute__((unused)) void *x)
-{
-    while (1) {
-        int nevents, ret;
-
-        nevents = poll(&pfd, 1, -1);
-
-        if (nevents == -1) {
-            if (errno == EINTR)
-                continue;
-            ALOGE("powerhal: thread_uevent: poll_wait failed\n");
-            break;
-        }
-        ret = uevent_event();
-        if (ret < 0)
-            ALOGE("Error processing the uevent event");
-    }
-    return NULL;
-}
-
-static void uevent_init()
-{
-    struct sockaddr_nl client;
-    pthread_t tid;
-    pfd.fd = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
-
-    if (pfd.fd < 0) {
-        ALOGE("%s: failed to open: %s", __func__, strerror(errno));
-        return;
-    }
-    memset(&client, 0, sizeof(struct sockaddr_nl));
-    pthread_create(&tid, NULL, thread_uevent, NULL);
-    client.nl_family = AF_NETLINK;
-    client.nl_pid = tid;
-    client.nl_groups = -1;
-    pfd.events = POLLIN;
-    bind(pfd.fd, (void *)&client, sizeof(struct sockaddr_nl));
-    return;
-}
-
 static void power_init(__attribute__((unused)) struct power_module *module)
 {
+    /* TODO: Add support for this? */
     ALOGI("%s", __func__);
-    socket_init();
-    uevent_init();
 }
-
-static void sync_thread(int off)
-{
-    int rc;
-    pid_t client;
-    char data[MAX_LENGTH];
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    client = getpid();
-
-    if (!off) {
-        snprintf(data, MAX_LENGTH, "2:%d", client);
-        rc = sendto(client_sockfd, data, strlen(data), 0,
-            (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    } else {
-        snprintf(data, MAX_LENGTH, "3:%d", client);
-        rc = sendto(client_sockfd, data, strlen(data), 0,
-            (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    }
-
-    if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
-    }
-}
-
-static void enc_boost(int off)
-{
-    int rc;
-    pid_t client;
-    char data[MAX_LENGTH];
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    client = getpid();
-
-    if (!off) {
-        snprintf(data, MAX_LENGTH, "5:%d", client);
-        rc = sendto(client_sockfd, data, strlen(data), 0,
-            (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    } else {
-        snprintf(data, MAX_LENGTH, "6:%d", client);
-        rc = sendto(client_sockfd, data, strlen(data), 0,
-            (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    }
-
-    if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
-    }
-}
-
-static void process_video_encode_hint(void *metadata)
-{
-
-    socket_init();
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    if (metadata) {
-        if (!strncmp(metadata, STATE_ON, sizeof(STATE_ON))) {
-            /* Video encode started */
-            sync_thread(1);
-            enc_boost(1);
-        } else if (!strncmp(metadata, STATE_OFF, sizeof(STATE_OFF))) {
-            /* Video encode stopped */
-            sync_thread(0);
-            enc_boost(0);
-        }  else if (!strncmp(metadata, STATE_HDR_ON, sizeof(STATE_HDR_ON))) {
-            /* HDR usecase started */
-        } else if (!strncmp(metadata, STATE_HDR_OFF, sizeof(STATE_HDR_OFF))) {
-            /* HDR usecase stopped */
-        }else
-            return;
-    } else {
-        return;
-    }
-}
-
-
-#if 0
-static void touch_boost()
-{
-    int rc;
-    pid_t client;
-    char data[MAX_LENGTH];
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    client = getpid();
-
-    snprintf(data, MAX_LENGTH, "1:%d", client);
-    rc = sendto(client_sockfd, data, strlen(data), 0,
-        (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
-    }
-}
-#endif
 
 static void power_set_interactive(__attribute__((unused)) struct power_module *module, int on)
 {
-    if (last_state == -1) {
-        last_state = on;
-    } else {
-        if (last_state == on)
-            return;
-        else
-            last_state = on;
-    }
-
+    /* TODO: Add support for this? */
     ALOGV("%s %s", __func__, (on ? "ON" : "OFF"));
-    if (on) {
-        sync_thread(0);
-#if 0
-        touch_boost();
-#endif
-    } else {
-        sync_thread(1);
-    }
 }
 
 static void power_hint( __attribute__((unused)) struct power_module *module,
@@ -351,52 +89,33 @@ static void power_hint( __attribute__((unused)) struct power_module *module,
         case POWER_HINT_INTERACTION:
         case POWER_HINT_LAUNCH:
             ALOGV("POWER_HINT_INTERACTION");
-#if 0
-            touch_boost();
-#endif
-            break;
-#if 0
-        case POWER_HINT_VSYNC:
-            ALOGV("POWER_HINT_VSYNC %s", (data ? "ON" : "OFF"));
-            break;
-#endif
-        case POWER_HINT_VIDEO_ENCODE:
-            process_video_encode_hint(data);
+            if (!low_power_mode) /* No boost if low power mode is active. */
+                sysfs_write(BOOST_PATH, "1");
             break;
 
         case POWER_HINT_LOW_POWER:
-             pthread_mutex_lock(&low_power_mode_lock);
              ALOGI("POWER_HINT_LOW_POWER");
              if (data) {
                  low_power_mode = true;
-                 for (cpu = 0; cpu < TOTAL_CPUS; cpu++) {
-                     sysfs_write(cpu_path_min[cpu], LOW_POWER_MIN_FREQ);
-                     ret = sysfs_write(cpu_path_max[cpu], LOW_POWER_MAX_FREQ);
-                     if (!ret) {
-                         freq_set[cpu] = true;
-                     }
-                 }
+                 sysfs_write(LIMIT_PATH, LIMIT_FREQ);
              } else {
                  low_power_mode = false;
-                 for (cpu = 0; cpu < TOTAL_CPUS; cpu++) {
-                     ret = sysfs_write(cpu_path_max[cpu], NORMAL_MAX_FREQ);
-                     if (!ret) {
-                         freq_set[cpu] = false;
-                     }
-                 }
+                 sysfs_write(LIMIT_PATH, NORMAL_FREQ);
              }
-             pthread_mutex_unlock(&low_power_mode_lock);
              break;
+
         default:
              break;
     }
 }
 
-static int power_open(const hw_module_t *module, const char *name,
+static int power_open(__attribute__((unused)) const hw_module_t *module, const char *name,
                             hw_device_t **device)
 {
     ALOGD("%s: enter; name=%s", __FUNCTION__, name);
+
     int retval = 0; /* 0 is ok; -1 is error */
+
     if (strcmp(name, POWER_HARDWARE_MODULE_ID) == 0) {
         power_module_t *dev = (power_module_t *)calloc(1,
                 sizeof(power_module_t));
@@ -414,6 +133,7 @@ static int power_open(const hw_module_t *module, const char *name,
     } else {
         retval = -EINVAL;
     }
+
     ALOGD("%s: exit %d", __FUNCTION__, retval);
     return retval;
 }
