38cda1570ce4ad2d31ed3c21ce1b982002c37877
voron00 <alexey12424323@gmail.com>
2018-04-03 21:10:33 +0300

    mako: set build fingerprint correctly
    Fixes system and vendor fingerprint mismatch after https://review.lineageos.org/#/c/208868/
    
    Change-Id: I0f6676b2f54a77ce498dbb70e53cbdea8bcaaf9d

--- lineage.mk
+++ lineage.mk
@@ -19,5 +19,7 @@ PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48T/2237560:user/release-keys \
+    PRODUCT_NAME=occam \
     PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48T 2237560 release-keys"
+
+BUILD_FINGERPRINT := google/occam/mako:5.1.1/LMY48T/2237560:user/release-keys
