8b6ddb9dcaf683844748504cc5806ae90c52f5de
voron00 <alexey12424323@gmail.com>
2018-04-30 10:56:53 +0300

    mako: fix minor typo in device.mk
    Change-Id: Ieb298af17c7e5a5e4f0d401cead32904b2e54ea9

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -98,7 +99,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-impl
 
 # Graphics
 PRODUCT_PACKAGES += \
